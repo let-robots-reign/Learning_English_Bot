18c2aff25606b3bb0dc7c1bcc6b2ee90ad3dfc26
let-robots-reign <zotov.alexei.1@yandex.ru>
2018-05-17 15:33:39 +0400

    trying to fix things

--- english_bot.py
+++ english_bot.py
@@ -49,17 +49,6 @@ def setting_up(bot, update):
     try:
         data_base = DataBase(update.message.from_user.id)
         data_base.create_table()
-        if update.message.from_user.id == 590585095:
-            users = data_base.select_users()
-            for i in range(len(users)):
-                #print(users[i])
-                user = int(users[i][0].split("_")[1])
-                #print(user)
-                bot.send_message(chat_id=user, text='Уважаемый пользователь, просим прощения за многочисленные ошибки. '
-                                                    'Сейчас мы надеемся, что бот будет работать исправно у всех пользователей.\n'
-                                                    'Enjoy your learning!')
-                bot.send_photo(chat_id=user,
-                               photo="http://www.broadway.kiev.ua/wp-content/uploads/2017/04/motivation-to-learn-english-e1491480111422.jpg")
     except Exception as e:
         bot.send_message(chat_id=590585095, text=e)
         update.message.reply_text('Sorry, error while reading data base')
