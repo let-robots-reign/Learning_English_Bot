a8aa28232728529dcc980a59b1a83d6561d9fc68
let-robots-reign <zotov.alexei.1@yandex.ru>
2018-05-17 14:52:50 +0400

    check image sender

--- english_bot.py
+++ english_bot.py
@@ -43,12 +43,15 @@ def setting_up(bot, update):
     try:
         data_base = DataBase(update.message.from_user.id)
         data_base.create_table()
-        # if update.message.from_user.id == 590585095:
-        #     users = data_base.select_users()
-        #     for i in range(len(users)):
-        #         user = int(users[i][0].split("_")[1])
-        #         bot.send_message(chat_id=user, text='Работа бота ненадолго приостановлена.\n'
-        #                                             'Просим прощения за неудобства.')
+        if update.message.from_user.id == 590585095:
+            users = data_base.select_users()
+            for i in range(len(users)):
+                user = int(users[i][0].split("_")[1])
+                if user == 590585095:
+                    bot.send_message(chat_id=user, text='Работа бота восстановлена.\n'
+                                                        'Чтобы продолжить работу со мной, введите /start.')
+                    bot.send_photo(chat_id=user,
+                                   photo="https://realenglishconversations.files.wordpress.com/2014/12/inspirational-language-quote.png?w=640")
     except:
         update.message.reply_text('Sorry, error while reading data base')
         return TRANSLATE
